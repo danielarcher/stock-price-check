49c2a1ed6773cb86f57446e27e8fc7639f131e17
Daniel Archer <contato@danielcramer.com.br>
2021-11-16 17:14:33 -0500

    add failure test case

--- tests/Feature/GetStockPriceTest.php
+++ tests/Feature/GetStockPriceTest.php
@@ -9,11 +9,6 @@ class GetStockPriceTest extends TestCase
 {
     const TEST_STOCK = 'AMZN';
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function test_should_return_ok_when_check_stock()
     {
         $response = $this->get('/api/check-stock/' . self::TEST_STOCK);
@@ -21,9 +16,17 @@ class GetStockPriceTest extends TestCase
         $response->assertStatus(200);
     }
 
+    public function test_should_return_not_found_when_incorrect_stock()
+    {
+        $response = $this->get('/api/check-stock/XXXX');
+
+        $response->assertStatus(404);
+    }
+
     public function test_should_return_correct_data_structure()
     {
         $response = $this->get('/api/check-stock/' . self::TEST_STOCK);
+
         self::assertEquals(['symbol','high','low','price'],array_keys($response->json()));
     }
 }
