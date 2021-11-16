65c9e4efb91371b4239ad322883df41ecb73041c
Daniel Archer <contato@danielcramer.com.br>
2021-11-16 17:13:32 -0500

    docs

--- src/Domain/StockQuoteService.php
+++ src/Domain/StockQuoteService.php
@@ -3,11 +3,22 @@
 namespace ThreePriceChecker\Domain;
 
 use ThreePriceChecker\Domain\Exception\StockQuoteNotFound;
+use ThreePriceChecker\Domain\Exception\StockQuoteServiceNotFunctional;
 
+/**
+ * Interface StockQuoteService
+ *
+ * @package ThreePriceChecker\Domain
+ */
 interface StockQuoteService
 {
     /**
+     * @param string $symbol
+     *
+     * @return StockQuote
+     *
      * @throws StockQuoteNotFound
+     * @throws StockQuoteServiceNotFunctional
      */
     public function get(string $symbol): StockQuote;
 }
